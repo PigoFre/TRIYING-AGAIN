84dc712c060ac425a91e11a4f6221996579fc91d
PigoFre <78384268+PigoFre@users.noreply.github.com>
2023-12-12 18:30:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from your_gpt_script import process_user_input
 
 app = Flask(__name__)
+app.secret_key = '1355'  # Set a secret key for session management
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    authenticated = False  # Variable to track if the user is authenticated
-    error_message = None
-    response = ""
+    if 'authenticated' not in session:
+        session['authenticated'] = False
 
     if request.method == "POST":
         if 'password' in request.form:
@@ -15,15 +15,17 @@ def home():
             correct_password = '15399'  # Replace with your actual password
 
             if password == correct_password:
-                authenticated = True
+                session['authenticated'] = True
+                return redirect(url_for('home'))
             else:
-                error_message = "Incorrect password, try again."
+                return render_template('index.html', error="Incorrect password, try again.")
 
-        elif 'user_input' in request.form:
+        elif 'user_input' in request.form and session.get('authenticated'):
             user_input = request.form["user_input"]
             response = process_user_input(user_input)
+            return jsonify({'response': response})
 
-    return render_template("index.html", authenticated=authenticated, error=error_message, response=response)
+    return render_template("index.html", authenticated=session['authenticated'])
 
 if __name__ == "__main__":
     app.run(debug=True)
